ce7d406623c7e742e89e6541fea5982a09faecaa
DevTatsuya <pikachutrained@icloud.com>
2023-04-23 15:37:50 -0500

    We Finished

--- app/src/main/java/com/example/replacement/GameScreen.kt
+++ app/src/main/java/com/example/replacement/GameScreen.kt
@@ -2,28 +2,63 @@ package com.example.replacement
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-var Score: Int=0
+
 @Composable
 fun GameScreen(navController: NavController){
-    Text("Score: $Score", fontSize = 55.sp)
-   Image(painter = painterResource(id = R.drawable.gamecandy), contentDescription = null,
-   modifier = Modifier
-       .width(1000.dp)
-       .height(1000.dp)
-       .clickable {
-           Score++
-       }
-   )
+    var clicks: MutableState<Int> = remember { mutableStateOf(1) }
+   var upgrade1: Int = 0
+    var upgrade2: Int = 0
+    var score : MutableState<Int> = remember { mutableStateOf(0)}
+    Column() {
+        Row() {
+            Text("Candies: ${score.value}", fontSize = 55.sp)
+        }
+        Row() {
+            Column(modifier = Modifier.padding(8.dp)) {
+                Button(
+                    onClick = { if(score.value >= 10){
+                        score.value = score.value - 10
+                        clicks.value= clicks.value+1}}
+                ) {
+                    Text(text = "Cost:10 Candies\n+1 Candies/Click")
+                }
+            }
+
+            Column(modifier = Modifier.padding(8.dp)) {
+                Button(
+                    onClick = { if(score.value >= 100){
+                        score.value = score.value - 100
+                        clicks.value = clicks.value + 10}}
+                ) {
+                    Text(text = "Cost:100 Candies\n+10 Candies/Click")
+                }
+            }
+        }
+        Row() {
+            Image(painter = painterResource(id = R.drawable.gamecandy), contentDescription = null,
+                modifier = Modifier
+                    .width(1000.dp)
+                    .height(1000.dp)
+                    .clickable {
+
+                        score.value = score.value + clicks.value
+                    }
+            )
+        }
+
+    }
 }
